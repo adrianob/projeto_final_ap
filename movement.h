d92acc1a4a41eaf241b87a63d49927d9e6c4e027
adriano benin <adriano.benin@gmail.com>
2014-10-14 17:07:03 -0300

    refactor sprites and move some functions to sprites file

--- movement.h
+++ movement.h
@@ -4,18 +4,18 @@ struct game_state game_state;
 
 void print_char(WINDOW *w, sprite* sprite);
 void debug_print(WINDOW *w, sprite* md, sprite* nest);
-void move_ghost(WINDOW *w, struct ghost* gh);
-void move_rock(WINDOW *w, struct rock* rk);
-void shoot(WINDOW *w, struct shot* s);
+void move_ghost(WINDOW *w, sprite *gh);
+void move_rock(WINDOW *w, sprite *rk);
+void shoot(WINDOW *w, sprite* s);
 int can_go_to_direction(WINDOW *w, struct position* p, int direction);
 int can_fall(WINDOW *w, struct position* p, int direction);
 void move_sprite(WINDOW *w, sprite* sprite, int direction);
 void move_if_possible(WINDOW *w, sprite* s);
 void check_collision(WINDOW *w, sprite *sp1, sprite *sp2);
 void check_shot_collision(WINDOW *w, sprite *sp1, sprite *sp2);
-void move_ghosts(WINDOW *w, struct ghost ghosts[MAX_GHOSTS]);
-void move_rocks(WINDOW *w, struct rock rocks[MAX_ROCKS]);
-int next_char(WINDOW *w, struct position* p, int direction);
+void move_ghosts(WINDOW *w, sprite ghosts[MAX_GHOSTS]);
+void move_rocks(WINDOW *w, sprite rocks[MAX_ROCKS]);
+int next_char(WINDOW *w, struct position *p, int direction);
 void check_fruit_collision(WINDOW *w, sprite* sprite, int direction);
 
 #endif
