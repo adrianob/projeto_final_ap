f6d2474b415d2932a248c12bc634fe4aba5bf1f8
Dalalibera <giovannedalalibera@hotmail.com>
2014-11-13 15:06:10 -0200

    Update game.c

--- game.c
+++ game.c
@@ -226,13 +226,11 @@ void check_score(int score){
     printw( "Informe seu nome:\n");
     refresh();
     nocbreak();
-    echo();
     scanf("%12[^\n]",highscores[TOP_SCORES].name);
     //fgets(highscores[TOP_SCORES].name,MAX_NAME,stdin);
     scanf("%*[^\n]"); scanf("%*c");
 
     cbreak();
-    noecho();
   }
   qsort((void *) &highscores,              // Endereço do primeiro elemento do array
    6,                                       // Número de elementos do array
